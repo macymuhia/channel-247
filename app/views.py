2dd4ce865522e0d1929efad205db807007577e10
macymuhia <macymuhia@gmail.com>
2019-07-30 13:02:55 +0300

    Add views file that defines the logic for data to be displayed

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from flask import render_template
+from app import app
+
+# from .request import get_news
+from .request import get_news, get_articles
+
+# Views
+@app.route("/")
+def index():
+
+    """
+    View root page function that returns the index page and its data
+    """
+
+    # Getting business news
+    business_news = get_news("business")
+    technology_news = get_news("technology")
+    sports_news = get_news("sports")
+    general_news = get_news("general")
+    entertainment_news = get_news("entertainment")
+    title = "Home - Welcome to News"
+    return render_template(
+        "index.html",
+        title=title,
+        business=business_news,
+        technology=technology_news,
+        sports=sports_news,
+        general=general_news,
+        entertainment=entertainment_news,
+    )
+
+
+@app.route("/article/<id>")
+def article(id):
+
+    """
+    View article  page function that returns the article details page and its data
+    """
+    articles = get_articles(id)
+    title = id
+    return render_template("article.html", title=title, articles=articles)
+
